bd6a9b43aeee6cf5b249e9c6fec8eaaf8f458b0d
Mourad Essebane <essebane.mourad@gmail.com>
2020-01-22 11:45:06 +0100

    Modified Input when no args given

--- .my_commands.sh
+++ .my_commands.sh
@@ -8,30 +8,30 @@ if [ "$#" -gt 0 ]
 then
 	if [ -d $path"$1" ]
 	then
-	echo "Project exists Already"
+		echo "Project exists Already"
 	else
-	mkdir $path"$1"
-	cd $path"$1"
-	touch "app.js"
-	touch "README.md"
-	touch ".gitignore"
-	mkdir "routes/"
-	mkdir "models/"
-	mkdir "controllers/"
-        mkdir -p "views/includes"
-	touch "views/includes/header.ejs"
-	touch "views/includes/footer.ejs"
+		mkdir $path"$1"
+		cd $path"$1"
+		touch "app.js"
+		touch "README.md"
+		touch ".gitignore"
+		mkdir "routes/"
+		mkdir "models/"
+		mkdir "controllers/"
+			mkdir -p "views/includes"
+		touch "views/includes/header.ejs"
+		touch "views/includes/footer.ejs"
 
-	git init
-	git config --global user.name "Mourad Essebane"
-	git config --global user.email "essebane.mourad@gmail.com"
-	git add .
-	git commit -m "Initial commit"
-	echo "Project Directory created"
+		git init
+		git config --global user.name "Mourad Essebane"
+		git config --global user.email "essebane.mourad@gmail.com"
+		git add .
+		git commit -m "Initial commit"
+		echo "Project Directory created"
 	fi
 else
 	echo "no Args. Give name"
-	read  newPath
+	read -r newPath
 	bash create_nodejs.sh $newPath
 fi
 }
@@ -41,24 +41,24 @@ if [ "$#" -gt 0 ]
 then
 	if [ -d $path"$1" ]
 	then
-	echo "Project exists Already"
+		echo "Project exists Already"
 	else
-	mkdir $path"$1"
-	cd $path"$1"
+		mkdir $path"$1"
+		cd $path"$1"
 
-	touch "README.md"
-	touch ".gitignore"
+		touch "README.md"
+		touch ".gitignore"
 
-	git init
-	git config --global user.name "Mourad Essebane"
-	git config --global user.email "essebane.mourad@gmail.com"
-	git add .
-	git commit -m "Initial commit"
-	echo "Project Directory created"
+		git init
+		git config --global user.name "Mourad Essebane"
+		git config --global user.email "essebane.mourad@gmail.com"
+		git add .
+		git commit -m "Initial commit"
+		echo "Project Directory created"
 	fi
 else
 	echo "no Args. Give name"
-	read  newPath
+	read  -r newPath
 	bash create.sh $newPath
 fi
 }
