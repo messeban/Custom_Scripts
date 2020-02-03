a1b2bfd762e1dbb964670d0f97b6519f35ab7b54
Mourad Essebane <essebane.mourad@gmail.com>
2020-02-03 19:01:32 +0100

    Centralized all create functions in one

--- .my_commands.sh
+++ .my_commands.sh
@@ -2,66 +2,60 @@
 
 path="/home/essebane0ne/Documents/Projects/"
 
-function create_nodejs(){
-
-		if [ "$#" -gt 0 ]
-		then
-			if [ -d $path"$1" ]
-			then
-				echo "Project exists Already"
-			else
-				mkdir $path"$1"
-				cd $path"$1"
-				touch "app.js"
-				touch "README.md"
-				touch ".gitignore"
-				mkdir routes/ models/ controllers/ util/
-				mkdir -p "public/css/"
-				mkdir "public/js"
-				mkdir -p "views/includes"
-				touch "views/includes/header.ejs"
-				touch "views/includes/footer.ejs"
-
-				git init
-				git config --global user.name "Mourad Essebane"
-				git config --global user.email "essebane.mourad@gmail.com"
-				git add .
-				git commit -m "Initial commit"
-				code .
-				google-chrome https://www.github.com/messeban/ </dev/null >/dev/null 2>&1 & disown
-				echo "Project Directory created"
-
-			fi
-		else
-			read -p "no Args. Give name: " opt
-			create opt
-		fi
-}
 function create(){
-
-		if [ "$#" -gt 0 ]
+	printf "Make a choice:\n"
+	type=("NodeJS" "Bash" "Html")
+	len=${#type[@]}
+	for((i=0; i<len; i++))
+		do 
+		j=$(expr $i + 1)
+		printf "\t"
+		echo "[$j] ${type[$i]}"
+		done
+	read -p "Choose Programming Language:" choice
+	k=$(expr $choice - 1)
+	if [ $choice -gt 0 ]
 		then
-			if [ -d $path"$1" ]
-			then
-				echo "Project exists Already"
-			else
-				mkdir $path"$1"
-				cd $path"$1"
-
-				touch "README.md"
-				touch ".gitignore"
-
-				git init
-				git config --global user.name "Mourad Essebane"
-				git config --global user.email "essebane.mourad@gmail.com"
-				git add .
-				git commit -m "Initial commit"
-				echo "Project Directory created"
+			printf "You chose ${type[$k]}\nConfirm[Y/n]?\n"
+			read conf
+			confirm=${conf,,}
+			if [ $confirm = "y" ]
+				then
+					printf "You confirmed ${type[$k]}\n"
+					read -p "Project Name: " name
+					if [ ! -z "$name" ]
+						then
+								if [ -d $path"$name" ]
+								then
+									echo "$path$name"
+									echo "Project exists Already"
+								else
+									case $choice in
+										1)
+											echo "end of nodejs"
+											make_nodejs "$name"
+											;;
+										2)
+											echo "Bash"
+											make "$name"
+											;;
+										3)
+											echo "Htt"
+											;; 
+										*)
+											echo "Hmm, seems i've never used it."
+											;;
+									esac
+									fi
+					else
+						read -p "no Args. Give name: " opt
+						create opt
+					fi
 			fi
 		else
-			read -p "no Args. Give name: " opt
-			create "$opt"
+			printf "You chose Bash\n"
 		fi
+		
 }
 
 
@@ -125,4 +119,42 @@ function google(){
 			google-chrome https://www.google.com/ </dev/null >/dev/null 2>&1 & disown
 			clear
 		fi
+}
+function make_nodejs(){
+	mkdir $path"$1"
+	cd $path"$1"
+	touch "app.js"
+	touch "README.md"
+	touch ".gitignore"
+	mkdir routes/ models/ controllers/ util/
+	mkdir -p "public/css/"
+	mkdir "public/js"
+	mkdir -p "views/includes"
+	touch "views/includes/header.ejs"
+	touch "views/includes/footer.ejs"
+
+	git init
+	git config --global user.name "Mourad Essebane"
+	git config --global user.email "essebane.mourad@gmail.com"
+	git add .
+	git commit -m "Initial commit"
+	code .
+	google-chrome https://www.github.com/messeban/ </dev/null >/dev/null 2>&1 & disown
+	echo "Project Directory created"
+}
+
+function make(){
+	mkdir $path"$1"
+	cd $path"$1"
+
+	touch "README.md"
+	touch ".gitignore"
+
+	git init
+	git config --global user.name "Mourad Essebane"
+	git config --global user.email "essebane.mourad@gmail.com"
+	git add .
+	git commit -m "Initial commit"
+	code .
+	echo "Project Directory created"
 }
